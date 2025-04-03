546d477342e4ef8cbae3346e1bf2f36b1595df1f
GaetanoNicotra <nicotragaetano10@gmail.com>
2025-04-03 15:44:45 +0200

    [UPD] Stampata in console la mail ricavata dall'API

--- js/script.js
+++ js/script.js
@@ -1,4 +1,13 @@
+// recupero gli elementi dal dom
 
+const list = document.getElementById('list');
+const listElement = document.querySelector('.list-element')
+
+// utilizzo il linguaggio axios per recuperare le mail random dall'apposita API
 axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((element) => {
     console.log(element.data.response)
+for(let i=0; i<=10; i++){
+     listElement.innerHTML = element.data.response;
+}
+   
 })
