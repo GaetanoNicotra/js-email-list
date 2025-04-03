01db52e934251039a65db7f0585b9cf46d834f95
GaetanoNicotra <nicotragaetano10@gmail.com>
2025-04-03 16:59:29 +0200

    [DONE] Esercizio terminato

--- js/script.js
+++ js/script.js
@@ -4,13 +4,13 @@ const list = document.getElementById('list');
 const listElement = document.querySelector('.list-element')
 
 // utilizzo il linguaggio axios per recuperare le mail random dall'apposita API
+ for (let i = 0; i <= 10; i++) {
 axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then(element => {
-    debugger
+
     console.log(element.data.response)
     
     ciao = element.data.response;
-    for (let i = 0; i <= 10; i++) {
-        list.innerHTML = `<li class="list-element">${ciao}</li>`;
-    }
-
+   
+        list.innerHTML += `<li class="list-element">${ciao}</li>`;
 })
+}
