fa1ae541ca10150b82a2ad9d6cdee91347f6754b
GaetanoNicotra <nicotragaetano10@gmail.com>
2025-04-03 18:05:02 +0200

    [UPD] bonus 2 completato

--- js/script.js
+++ js/script.js
@@ -2,14 +2,22 @@
 
 const list = document.getElementById('list');
 const listElement = document.querySelector('.list-element')
+const button = document.getElementById('button')
 
-// creo un ciclo for per generare 10 mail randomiche
-for (let i = 0; i <= 10; i++) {
-    // utilizzo il linguaggio axios per recuperare le mail random dall'apposita API
-    axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then(element => {
-        console.log(element.data.response)
-        // genero all'interno del DOM gli elementi della <ul>
-        elementoGenerato = element.data.response;
-        list.innerHTML += `<li class="list-element">${elementoGenerato}</li>`;
-    })
+// creo una funzione per generare al click del bottone 10 nuove mail
+const generateNewMail = () => {
+    list.innerHTML = '';
+    // creo un ciclo for per generare 10 mail randomiche
+    for (let i = 0; i < 10; i++) {
+        // utilizzo il linguaggio axios per recuperare le mail random dall'apposita API
+        axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then(element => {
+            console.log(element.data.response)
+            // genero all'interno del DOM gli elementi della <ul>
+            elementoGenerato = element.data.response;
+            list.innerHTML += `<li class="list-element">${elementoGenerato}</li>`;
+        })
+    }
 }
+// creo l'evento da verificarsi al click
+button.addEventListener('click', generateNewMail);
+generateNewMail()
